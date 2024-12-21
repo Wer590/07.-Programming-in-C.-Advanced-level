e3785b368c255d0eec9f9fac1568b19451b763dc
Wer590 <wer59232@gmail.com>
2024-12-21 14:27:43 +0000

    modified:   a.out 	modified:   main.c

--- HW07/D1/main.c
+++ HW07/D1/main.c
@@ -1,10 +1,75 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "ctype.h"
 
+typedef struct tree 
+{
+	int key;
+	struct tree *left, *right;
+} tree;
 
+void insert(tree **root, int key)
+{
+	if(!(*root))
+	{
+		*root = (tree*) malloc(sizeof(tree));
+		(*root)->key = key;
+		(*root)->left = (*root)->right = NULL;
+	}
+	else if (key < ((*root)->key))
+		insert(&((*root)->left), key);
+	else
+		insert(&((*root)->right), key);
+}
+
+void btUpView(tree *root)
+{
+	if (!root)
+	{
+		return;
+	}
+
+	void inorder(tree *root)
+	{
+		if(root == NULL)
+			return;
+		if(root->left)
+		{
+			inorder(root->left);
+		}
+		printf("%d ", root->key);
+}
+	void outorder(tree *root, int key)
+	{
+		if( root == NULL)
+			return;
+		if (key == 1)
+			printf("%d ", root->key);
+		if((root->right))
+			outorder(root->right, 1);
+	}
+	inorder(root);
+
+	outorder(root, 0);
+	printf("\n");
+}
 
 int main()
 {
+	tree *tr = NULL;
+	insert(&tr, 10);
+	insert(&tr, 5);
+	insert(&tr, 15);
+	insert(&tr, 3);
+	insert(&tr, 7);
+	insert(&tr, 13);
+	insert(&tr, 18);
+	insert(&tr, 1);
+	insert(&tr, 6);
+	insert(&tr, 14);
+	insert(&tr, 0);
+
+	btUpView(tr);
 
 	return 0;
 }
